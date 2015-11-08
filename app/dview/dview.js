fac054c90a466399cc29f469fa3205d66f1aa74f
Tuomas Reunanen <tuomas.reunanen@aalto.fi>
2015-11-08 03:04:16 +0200

    front side data sending done

--- app/dview/dview.js
+++ app/dview/dview.js
@@ -1,18 +1,9 @@
 var debts = angular.module("debts", ['ngResource', 'ngAnimate', 'ui.bootstrap']);
 
 var cuid = 1;
-//var address = "http://83.136.252.107:9000/"
-var address = "http://localhost:9000/";
-var persons = []
-/*
-var persons = [
-  {name: 'Antonio', debt: 25.55},
-  {name: 'Boo-so', debt: -5.76},
-  {name: 'Skele-Toni', debt: -14.22},
-  {name: 'Tume', debt: 12.42},
-  {name: 'Emma', debt: -6.34},
-  {name: 'Late', debt: 99.99}
-];*/
+var address = "http://83.136.252.107:9000/";
+//var address = "http://localhost:9000/";
+var persons = [];
 
 var contacts = [
   {name: 'Antonio', id: 20},
@@ -37,7 +28,7 @@ debts.factory('userDetail', function( $resource ) {
     return $resource(address + "api/users/:id", {id: '@userID'});
 });
 
-debts.controller("DebtPersonController", function ($scope, $timeout, $dialog) {
+debts.controller("DebtPersonController", function ($scope, $timeout, $dialog, saldo, userDetail) {
 
   $scope.persons = persons;
 
@@ -64,17 +55,34 @@ debts.controller("DebtPersonController", function ($scope, $timeout, $dialog) {
     m.open('modal.html');
   };
 
+  $scope.loadData = function() {
+    $scope.persons = [];
+    saldo.query(function(data) {
+      angular.forEach(data, function(d){
+        userDetail.get({id: d.id}).$promise.then(function(data){
+          $scope.persons.push({name: data.username, debt:d.saldo });
+        });
+      });
+    });
+  };
+
   // $dialog.dialog({}).open('modalContent.html');
 
   $scope.$on("viewChangeEvent", function (event, args) {
     m.close();
+    $scope.loadData();
   });
 });
 
-debts.controller("EventController", function ($scope, $rootScope, $http, saldo, userDetail) {
+debts.controller("EventController", function ($scope, $rootScope, $http) {
   $scope.items = items;
   $scope.contacts = contacts;
   $scope.selectedContacts = {};
+
+  $scope.transTargetId = 2;
+  $scope.transAmount = 15;
+  $scope.transTag = 'Food';
+
 
   $scope.addItem = function() {
       var sel = [];
@@ -90,16 +98,37 @@ debts.controller("EventController", function ($scope, $rootScope, $http, saldo, 
       $scope.selectedContacts = {};
   };
 
+  $scope.sendTransaction = function() {
+    var payer = $scope.transTargetId;
+    var receiver = cuid;
+    var amount = $scope.transAmount;
+    if (amount < 0) {
+      amount = -amount;
+      var tmp = payer;
+      payer = receiver;
+      receiver = tmp;
+    }
+    var formData = {
+      'payer'       : payer,
+      'receiver'    : receiver,
+      'amount'      : amount,
+      'tag'         : $scope.transTag
+    };
+
+    $.ajax({
+      type      : 'POST',
+      url       : address + 'api/transactions/',
+      data      : formData,
+      dataType  : 'json',
+      success   : function(data) {
+        console.log(data);
+      }
+    });
+    $rootScope.$broadcast("viewChangeEvent", {});
+  };
 
   $scope.acceptEvent = function() {
-    saldo.query(function(data) {
-      angular.forEach(data, function(d){
-        userDetail.get({id: d.id}).$promise.then(function(data){
-          persons.push({name: data.username, debt:d.saldo })
-        });
-      });
-    });
     $scope.items = [];
-    $rootScope.$broadcast("viewChangeEvent", {view: 0 });
+    $rootScope.$broadcast("viewChangeEvent", {});
   };
 });
